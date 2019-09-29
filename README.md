0c2663a66366cfe6965beb7ba459cde3535a7545
unknown <1danabana@gmail.com>
2019-09-28 20:10:07 -0700

    problems with mySQL require assistance

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # burger-logs
 an app for a fictional restaurant that lets you input the name of a burger you want to eat
+
+a problem with the mySQL has stopped me from continueing this excersie i will stay after too ask help for the rest of this project thank you for your time
--- controller/routes.js
+++ controller/routes.js
@@ -1,9 +1,13 @@
 // a file that routes all the actions from the other files together
 var express = require('express');
 var router = express.Router();
-
+var burger = require ('../models/burgers.js');
+// referecing the burgers.js file to pass the data through the function
 router.get('/',function(req,res){
-    res.render('index');
+    burger.all(function(burger_data){
+        console.log(burger_data);
+        res.render('index');
+    })
 })
 
 module.exports = router;
